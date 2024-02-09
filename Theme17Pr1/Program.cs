cd8dee8ccfcdbe773b04431833b57dd0bfd926dc
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-02-09 12:43:57 +0200

    Update Program.cs

--- Theme17Pr1/Program.cs
+++ Theme17Pr1/Program.cs
@@ -8,44 +8,53 @@ namespace Theme17Pr1
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            //Console.Write("Введите возраст ");
-            //int age = int.Parse(Console.ReadLine());
-            //Console.Write("Введите имя ");
-            //string name = Console.ReadLine();
-            //Console.Write("Введите фамилию ");
-            //string surname = Console.ReadLine();
-            //Console.Write("Введите название компании ");
-            //string company = Console.ReadLine();
-            //Console.Write("Введите название работы ");
-            //string job = Console.ReadLine();
-            //Person p1 = new Person(age);
-            //p1.Print();
-            //Console.WriteLine();
-            //Person p2 = new Person(age,name,surname);
-            //p2.Print();
-            //Employee p3 = new Employee(age,name,surname, company, job);
-            //p3.Print();
 
-            Console.Write("Введите кол-во копий ");
-            int copies = int.Parse(Console.ReadLine());
-            Console.Write("Введите название газеты ");
-            string name = Console.ReadLine();
-            Console.Write("Введите стоимость одной газеты ");
-            int price = int.Parse(Console.ReadLine());
-            Console.Write("Введите качество тиража(высокое/среднее/низкое) ");
-            string quality = Console.ReadLine();
-            Press n1 = new Press(copies,name,price);
-            n1.Output();
-            Magazine n2 = new Magazine(copies, name, price, quality);
-            n2.Output();
+            static void Main(string[] args)
+            {
+                //Console.Write("Введите возраст ");
+                //int age = int.Parse(Console.ReadLine());
+                //Console.Write("Введите имя ");
+                //string name = Console.ReadLine();
+                //Console.Write("Введите фамилию ");
+                //string surname = Console.ReadLine();
+                //Console.Write("Введите название компании ");
+                //string company = Console.ReadLine();
+                //Console.Write("Введите название работы ");
+                //string job = Console.ReadLine();
+                //Person p1 = new Person(age);
+                //p1.Print();
+                //Console.WriteLine();
+                //Person p2 = new Person(age,name,surname);
+                //p2.Print();
+                //Employee p3 = new Employee(age,name,surname, company, job);
+                //p3.Print();
+
+                //Console.Write("Введите кол-во копий ");
+                //int copies = int.Parse(Console.ReadLine());
+                //Console.Write("Введите название газеты ");
+                //string name = Console.ReadLine();
+                //Console.Write("Введите стоимость одной газеты ");
+                //int price = int.Parse(Console.ReadLine());
+                //Console.Write("Введите качество тиража(высокое/среднее/низкое) ");
+                //string quality = Console.ReadLine();
+                //Press n1 = new Press(copies, name, price);
+                //n1.Output();
+                //Magazine n2 = new Magazine(copies, name, price, quality);
+                //n2.Output();
+
+                Console.Write("Введите название тетради: ");
+                string name = Console.ReadLine();
+                Console.Write("Введите кол-во страниц: ");
+                int k = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите материал обложки: ");
+                string coverMaterial = Console.ReadLine();
+                Notebook b1 = new Notebook(name, k);
+                b1.Output();
+                GeneralNotebook b2 = new GeneralNotebook(name, k, coverMaterial);
+                b2.Output();
 
 
-
-            Console.ReadKey();
-
-
+                Console.ReadKey();
+            }
         }
-    }
-}
+    } 

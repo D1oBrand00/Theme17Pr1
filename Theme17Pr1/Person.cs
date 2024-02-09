67b305d6068a8f96d85ebe82219904c3c8cc3696
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-02-09 12:43:40 +0200

    Update Person.cs

--- Theme17Pr1/Person.cs
+++ Theme17Pr1/Person.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,27 +8,42 @@ namespace Theme17Pr1
 {
     class Person
     {
+        /// <summary>
+        /// Поле возраста
+        /// </summary>
         int age;
+        /// <summary>
+        /// Поле имени
+        /// </summary>
         string name;
+        /// <summary>
+        /// Поле фамилии
+        /// </summary>
         string surname;
 
+        /// <summary>
+        /// Свойства полей
+        /// </summary>
         public int Age { get => age; set => age = value; }
         public string Name { get => name; set => name = value; }
         public string Surname { get => surname; set => surname = value; }
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
         public Person() { }
         public Person(int age)
         {
             this.Age = age;
         }
-        public Person(int age,string name):
+        public Person(int age, string name) :
             this(age)
         {
             this.name = name;
         }
-        public Person(int age,string name, string surname):
-            this(age,name)
+        public Person(int age, string name, string surname) :
+            this(age, name)
         {
-            
+
             this.surname = surname;
         }
         public void Print()

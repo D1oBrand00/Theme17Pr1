d293f273c88b61b7b02503eb2fc3cd30aebf188a
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-02-09 08:08:12 +0200

    Update Magazine.cs

--- Theme17Pr1/Magazine.cs
+++ Theme17Pr1/Magazine.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,20 +11,34 @@ namespace Theme17Pr1
         string quality;
 
         public string Quality { get => quality; set => quality = value; }
-        public Magazine(int copies, string name, int price, string quality) :base(copies,name,price)
+        public Magazine(int copies, string name, int price, string quality) : base(copies, name, price)
         {
             this.quality = quality;
-            //if (quality == "высокое")
-            //{
-            //    base.Cost();
-            //    Console.WriteLine($"{Cost()}");
-            //}
+            
+        }
+          public double Cost()
+        {
+            if (quality == "высокое")
+            {
+                return  base.Cost() + (base.Cost() * 0.1);
+
+            }
+            else if (quality == "среднее")
+            {
+                return  base.Cost();
+            }
+            else if (quality == "низкое")
+            {
+                return base.Cost() - (base.Cost() * 0.1);
+            }
+            else
+            { return base.Cost(); }
         }
         public void Output()
         {
-            base.Cost();
-              
-            Console.WriteLine($"{Cost()}");
+            base.Output();
+
+            Console.WriteLine($"Качество тиража - {quality}\nНовая стоимость тиража - {Cost()}");
         }
     }
 }
